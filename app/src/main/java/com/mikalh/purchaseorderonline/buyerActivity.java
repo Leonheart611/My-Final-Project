591349145f9548418137f86f46f633a8635d39d1
Leonheart611 <mika.leonheart6@gmail.com>
2018-04-30 16:01:40 +0700

    add ketinggalan data

---
+++ app/src/main/java/com/mikalh/purchaseorderonline/buyerActivity.java
@@ -0,0 +1,42 @@
+package com.mikalh.purchaseorderonline;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mikalh.purchaseorderonline.Pager.BuyyerPagger;
+
+public class buyerActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+    TabLayout buyyerTab;
+    ViewPager buyyerView;
+    BuyyerPagger adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer);
+        buyyerTab = findViewById(R.id.buyyerTab);
+        buyyerView = findViewById(R.id.buyyerView);
+        adapter = new BuyyerPagger(getSupportFragmentManager());
+        buyyerView.setAdapter(adapter);
+        buyyerView.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(buyyerTab));
+        buyyerTab.setOnTabSelectedListener(this);
+
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        buyyerView.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
+}
