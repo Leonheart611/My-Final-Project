38418efb25d38475c5ac234761a3b63a90b89b55
Leonheart611 <mika.leonheart6@gmail.com>
2018-06-27 16:56:02 +0700

    Done update untuk yang Tampilan Admin

--- app/src/main/java/com/mikalh/purchaseorderonline/Profile.java
+++ app/src/main/java/com/mikalh/purchaseorderonline/Profile.java
@@ -21,6 +21,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -54,7 +57,8 @@ import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class Profile extends AppCompatActivity implements TabLayout.OnTabSelectedListener, View.OnClickListener{
+public class Profile extends AppCompatActivity implements TabLayout.OnTabSelectedListener, View.OnClickListener {
+    public static String IDSeller= "ID";
     TabLayout profile_tab;
     ViewPager profile_pagger;
     FloatingActionButton changImage_profile;
@@ -70,9 +74,10 @@ public class Profile extends AppCompatActivity implements TabLayout.OnTabSelecte
     int PICK_IMAGE_REQUEST = 1;
     String[] permissionsRequired = new String[]{android.Manifest.permission.CAMERA,
             android.Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-    Uri filePath,imageResult;
+    Uri filePath, imageResult;
     String ImageURL;
     User userModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -98,10 +103,10 @@ public class Profile extends AppCompatActivity implements TabLayout.OnTabSelecte
             }
         });
         changImage_profile = findViewById(R.id.changeImage_profile);
-        if (!user.getUid().equals(userID)){
+        if (!user.getUid().equals(userID)) {
             changImage_profile.hide();
         }
-        adapter = new ProfilePagger(getSupportFragmentManager(),2);
+        adapter = new ProfilePagger(getSupportFragmentManager(), 2);
         profile_pagger.setAdapter(adapter);
         profile_pagger.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(profile_tab));
         profile_tab.setOnTabSelectedListener(this);
@@ -122,7 +127,8 @@ public class Profile extends AppCompatActivity implements TabLayout.OnTabSelecte
     public void onTabReselected(TabLayout.Tab tab) {
 
     }
-    public void popUpAddFoto(){
+
+    public void popUpAddFoto() {
         final Dialog dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.choose_add_picture);
@@ -140,14 +146,14 @@ public class Profile extends AppCompatActivity implements TabLayout.OnTabSelecte
         cameraPicture_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!hasPermissions(Profile.this,permissionsRequired)) {
-                    requestPermissions(permissionsRequired,1);
-                }else {
+                if (!hasPermissions(Profile.this, permissionsRequired)) {
+                    requestPermissions(permissionsRequired, 1);
+                } else {
                     Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    File file = new File(Environment.getExternalStorageDirectory()+File.separator+"DCIM/Camera","IMG_lwlwlwlwlwwl.jpg");
+                    File file = new File(Environment.getExternalStorageDirectory() + File.separator + "DCIM/Camera", "IMG_lwlwlwlwlwwl.jpg");
                     filePath = Uri.fromFile(file);
                     i.putExtra(MediaStore.EXTRA_OUTPUT, filePath);
-                    startActivityForResult(i,0);
+                    startActivityForResult(i, 0);
                     dialog.dismiss();
                 }
 
@@ -174,21 +180,19 @@ public class Profile extends AppCompatActivity implements TabLayout.OnTabSelecte
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-                && data != null && data.getData() != null )
-        {
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
             filePath = data.getData();
             try {
                 final Bitmap bitmap;
-                bitmap = decodeSampledBitmapFromUri(filePath,imageProfile.getWidth(),imageProfile.getHeight());
+                bitmap = decodeSampledBitmapFromUri(filePath, imageProfile.getWidth(), imageProfile.getHeight());
                 imageProfile.setImageBitmap(bitmap);
                 uploadImage();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-        }if (requestCode == 0 && resultCode == RESULT_OK && filePath!=null){
+        }
+        if (requestCode == 0 && resultCode == RESULT_OK && filePath != null) {
             Matrix mat = new Matrix();
             try {
                 ExifInterface exif = new ExifInterface(filePath.getPath());
@@ -197,40 +201,41 @@ public class Profile extends AppCompatActivity implements TabLayout.OnTabSelecte
                 int rotateangle = 0;
                 if (orientation == ExifInterface.ORIENTATION_ROTATE_90)
                     rotateangle = 90;
-                if(orientation == ExifInterface.ORIENTATION_ROTATE_180)
+                if (orientation == ExifInterface.ORIENTATION_ROTATE_180)
                     rotateangle = 180;
-                if(orientation == ExifInterface.ORIENTATION_ROTATE_270)
+                if (orientation == ExifInterface.ORIENTATION_ROTATE_270)
                     rotateangle = 270;
-                mat.setRotate(rotateangle, (float) imageProfile.getWidth()/ 2, (float) imageProfile.getHeight() / 2);
+                mat.setRotate(rotateangle, (float) imageProfile.getWidth() / 2, (float) imageProfile.getHeight() / 2);
 
             } catch (IOException e) {
-                Log.e("Image Error path",e.getMessage());
+                Log.e("Image Error path", e.getMessage());
             }
             final Bitmap bitmap;
-            bitmap = decodeSampledBitmapFromUri(filePath,imageProfile.getWidth(),imageProfile.getHeight());
+            bitmap = decodeSampledBitmapFromUri(filePath, imageProfile.getWidth(), imageProfile.getHeight());
             imageProfile.setImageBitmap(bitmap);
             uploadImage();
         }
 
     }
 
-    public Bitmap decodeSampledBitmapFromUri(Uri uri,int reqWidth, int reqHeight){
+    public Bitmap decodeSampledBitmapFromUri(Uri uri, int reqWidth, int reqHeight) {
         Bitmap bm = null;
-        try{
+        try {
             final BitmapFactory.Options options = new BitmapFactory.Options();
             options.inJustDecodeBounds = true;
-            BitmapFactory.decodeStream(getContentResolver().openInputStream(uri),null ,options);
-            options.inSampleSize = calculateInSampleSize(options,reqWidth,reqHeight);
+            BitmapFactory.decodeStream(getContentResolver().openInputStream(uri), null, options);
+            options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
             options.inJustDecodeBounds = false;
 
             bm = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri)
-                    ,null, options);
+                    , null, options);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
         }
         return bm;
     }
+
     public int calculateInSampleSize(BitmapFactory.Options options,
                                      int reqWidth, int reqHeight) {
 // Raw height and width of image
@@ -246,14 +251,14 @@ public class Profile extends AppCompatActivity implements TabLayout.OnTabSelecte
         }
         return inSampleSize;
     }
+
     private void uploadImage() {
-        if(filePath != null)
-        {
+        if (filePath != null) {
             final ProgressDialog progressDialog = new ProgressDialog(this);
             progressDialog.setTitle("Uploading...");
             progressDialog.show();
 
-            final StorageReference ref = storageReference.child("images/"+ user.getUid());
+            final StorageReference ref = storageReference.child("images/" + user.getUid());
             UploadTask uploadTask = ref.putFile(filePath);
             Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
                 @Override
@@ -273,21 +278,21 @@ public class Profile extends AppCompatActivity implements TabLayout.OnTabSelecte
                         @Override
                         public void onFailure(@NonNull Exception e) {
                             Crashlytics.logException(e);
-                            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
                         }
                     });
-                    firestore.collection("Users").document(user.getUid()).update("url_pictLogo",imageResult.toString()).addOnFailureListener(new OnFailureListener() {
+                    firestore.collection("Users").document(user.getUid()).update("url_pictLogo", imageResult.toString()).addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-                                 Crashlytics.logException(e);
-                                 progressDialog.dismiss();
+                            Crashlytics.logException(e);
+                            progressDialog.dismiss();
                         }
                     }).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
 
                             progressDialog.dismiss();
-                            Toast.makeText(getApplicationContext(),"Suscess change Profile",Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "Suscess change Profile", Toast.LENGTH_LONG).show();
                         }
                     });
                 }
@@ -301,6 +306,7 @@ public class Profile extends AppCompatActivity implements TabLayout.OnTabSelecte
 
         }
     }
+
     public static boolean hasPermissions(Context context, String... permissions) {
         if (context != null && permissions != null) {
             for (String permission : permissions) {
@@ -311,13 +317,39 @@ public class Profile extends AppCompatActivity implements TabLayout.OnTabSelecte
         }
         return true;
     }
+
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Uri uri);
     }
+
     @Override
     public void onClick(View view) {
-        if (view == changeImage_profile){
+        if (view == changeImage_profile) {
             popUpAddFoto();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!user.getUid().equals(userID)) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.goto_catalogue, menu);
+            return super.onCreateOptionsMenu(menu);
+        } else {
+            return super.onCreateOptionsMenu(menu);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.catalogue_company:
+                Intent i = new Intent(getApplicationContext(), SearchResult.class);
+                i.putExtra(IDSeller, userID);
+                startActivity(i);
+                return true;
+            default:
+                return false;
         }
     }
 }
